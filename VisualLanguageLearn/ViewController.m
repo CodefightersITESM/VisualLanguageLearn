e6a2cc2e3fa6c7ce747479d1273fe72f211f43d5
Hector <diazhector98@gmail.com>
2018-08-25 14:44:36 -0500

    Object Detection from UIImages Established

--- VisualLanguageLearn/ViewController.m
+++ VisualLanguageLearn/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 @import Firebase;
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -18,17 +18,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    FIRVision *vision = [FIRVision vision];
-    FIRVisionLabelDetector *labelDetector = [vision labelDetector];
-    
-    UIImage *uiImage = [UIImage imageNamed:@"Tree"];
-    
-    FIRVisionImage *image = [[FIRVisionImage alloc] initWithImage:uiImage];
-    
-
+    [self takePicture];
 }
 
+-(void) takePicture {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+-(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    FIRVision *vision = [FIRVision vision];
+    FIRVisionLabelDetector *labelDetector = [vision labelDetector];
+    FIRVisionImage *image = [[FIRVisionImage alloc] initWithImage:editedImage];
+    [labelDetector detectInImage:image completion:^(NSArray<FIRVisionLabel *> * _Nullable labels, NSError * _Nullable error) {
+        if(error != nil){
+            NSLog(@"Error getting the labels");
+        } else {
+            
+            if(labels.count == 0){
+                NSLog(@"No labels detected");
+            } else {
+                for(FIRVisionLabel *label in labels){
+                    NSString *labelText = label.label;
+                    NSLog(@"Label Text: %@", labelText);
+                }
+            }
+        }
+    }];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
