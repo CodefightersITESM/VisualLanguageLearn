bd000901b7afb80410fed3925111766f6eba9057
César Barraza <cbarrazaa1@Cesars-Mac-mini.local>
2018-08-25 17:05:31 -0500

    Added translation manager

---
+++ VisualLanguageLearn/TranslationManager.m
@@ -0,0 +1,39 @@
+//
+//  TranslationManager.m
+//  VisualLanguageLearn
+//
+//  Created by César Barraza on 8/25/18.
+//  Copyright © 2018 Codefighters. All rights reserved.
+//
+
+#import "TranslationManager.h"
+#import <AFNetworking.h>
+
+static NSString* const API_KEY = @"AIzaSyCzwSy87KZ0AQJO6460slzTVLmt-5QLv8A";
+
+@implementation TranslationManager
++ (instancetype)shared {
+    static TranslationManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+- (void)getTranslation:(NSString *)text source:(NSString *)source target:(NSString *)target completion:(void (^)(NSString *))completion {
+    NSDictionary* params = @{@"q":text,
+                             @"target":target,
+                             @"format":@"text",
+                             @"source":source,
+                             @"key":API_KEY};
+    NSString* baseURL = @"https://translation.googleapis.com/language/translate/v2";
+    [self POST:baseURL parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSString* translation = [NSArray arrayWithArray:responseObject[@"data"][@"translations"]].firstObject[@"translatedText"];
+        completion(translation);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+        completion(nil);
+    }];
+}
+@end
